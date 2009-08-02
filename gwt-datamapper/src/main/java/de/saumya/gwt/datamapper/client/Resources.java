d91705fbfb679220980b3a2e3b1f235a11b0a792
mkristian <m.kristian@web.de>
2009-08-03 00:48:06 +0530

    made xml parsing more robust by omitting whitespace text nodes

--- gwt-datamapper/src/main/java/de/saumya/gwt/datamapper/client/Resources.java
+++ gwt-datamapper/src/main/java/de/saumya/gwt/datamapper/client/Resources.java
@@ -6,19 +6,19 @@ package de.saumya.gwt.datamapper.client;
 import java.util.ArrayList;
 import java.util.List;
 
-import de.saumya.gwt.datamapper.client.Resource.State;
-
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.xml.client.Element;
 import com.google.gwt.xml.client.NodeList;
 import com.google.gwt.xml.client.XMLParser;
 
+import de.saumya.gwt.datamapper.client.Resource.State;
+
 @SuppressWarnings("serial")
 public class Resources<E extends Resource<E>> extends ArrayList<E> {
 
-    private List<ResourcesChangeListener<E>> listeners = new ArrayList<ResourcesChangeListener<E>>();
+    private final List<ResourcesChangeListener<E>> listeners = new ArrayList<ResourcesChangeListener<E>>();
 
-    private final ResourceFactory<E>         factory;
+    private final ResourceFactory<E>               factory;
 
     // private final ResourceChangeListener<E> resourceChangeListener = new
     // ResourceChangeListener<E>(){
@@ -30,52 +30,56 @@ public class Resources<E extends Resource<E>> extends ArrayList<E> {
     //		
     // };
 
-    public Resources(ResourceFactory<E> factory) {
+    public Resources(final ResourceFactory<E> factory) {
         this.factory = factory;
     }
 
-    public void fromXml(String xml) {
+    public void fromXml(final String xml) {
         GWT.log(xml, null);
         fromXml(XMLParser.parse(xml).getDocumentElement());
-    }        
-     
-    public void fromXml(Element root) {            
-        NodeList list = root.getChildNodes();
-        for (int i = 0; i < list.getLength(); i++) {
-            E resource = factory.newResource();
-            resource.fromXml((Element) list.item(i));
-            resource.state = State.UP_TO_DATE;
-            add(resource);
-            fireResourcesChangeEvents(resource);
+    }
 
-            // resource.addResourceChangeListener(resourceChangeListener);
+    public void fromXml(final Element root) {
+        final NodeList list = root.getChildNodes();
+        for (int i = 0; i < list.getLength(); i++) {
+            final E resource = this.factory.newResource();
+            if (list.item(i) instanceof Element) {
+                resource.fromXml((Element) list.item(i));
+                resource.state = State.UP_TO_DATE;
+                add(resource);
+                fireResourcesChangeEvents(resource);
+
+                // resource.addResourceChangeListener(resourceChangeListener);
+            }
         }
     }
 
-    public String toXml(){
-        StringBuffer buf = new StringBuffer();
+    public String toXml() {
+        final StringBuffer buf = new StringBuffer();
         toXml(buf);
         return buf.toString();
-    }        
-    
-    public void toXml(StringBuffer buf){
-        buf.append("<").append(factory.storagePluralName()).append(">");
-        for(E rsrc: this){
+    }
+
+    public void toXml(final StringBuffer buf) {
+        buf.append("<").append(this.factory.storagePluralName()).append(">");
+        for (final E rsrc : this) {
             rsrc.toXml(buf);
         }
-        buf.append("</").append(factory.storagePluralName()).append(">");
+        buf.append("</").append(this.factory.storagePluralName()).append(">");
     }
-    
-    public void addResourcesChangeListener(ResourcesChangeListener<E> listener) {
+
+    public void addResourcesChangeListener(
+            final ResourcesChangeListener<E> listener) {
         this.listeners.add(listener);
     }
 
-    public void removeResourcesChangeListener(ResourcesChangeListener<E> listener) {
+    public void removeResourcesChangeListener(
+            final ResourcesChangeListener<E> listener) {
         this.listeners.remove(listener);
     }
 
-    private void fireResourcesChangeEvents(E resource) {
-        for (ResourcesChangeListener<E> listener : listeners) {
+    private void fireResourcesChangeEvents(final E resource) {
+        for (final ResourcesChangeListener<E> listener : this.listeners) {
             listener.onChange(this, resource);
         }
     }
