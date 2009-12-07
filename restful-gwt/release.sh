61bb2d4bfe92014d5c264f56f5c606a8bb7097eb
mkristian <m.kristian@web.de>
2009-12-07 12:10:45 +0530

    added for convinience

---
+++ restful-gwt/release.sh
@@ -0,0 +1,23 @@
+SNAPSHOT=$1
+VERSION=$2
+NEXT=$3
+if [ "x" = "x"$2 ] ; then
+    echo "usage: $0 SNAPHOT_VERSION VERSION NEXT_VERSION"
+    exit 1
+fi
+
+find -name "pom.xml" | xargs sed -i s/${SNAPSHOT}-SNAPSHOT/${VERSION}/
+find -name "pom.xml" | xargs git add
+git ci -m "release of version ${VERSION}" || exit
+git tag restful${VERSION}
+
+mvn clean deploy
+
+find -name "pom.xml" | xargs sed -i s/${VERSION}/${NEXT}-SNAPSHOT/
+
+find -name "pom.xml" | xargs git add
+git ci -m "next snapshot version ${NEXT}"
+
+mvn install
+
+git push --tags origin master
