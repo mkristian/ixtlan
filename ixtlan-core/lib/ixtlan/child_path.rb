aa3e7cb0ba7bfd733cd8d3a3e4799ea910e203a8
mkristian <m.kristian@web.de>
2010-01-25 13:30:34 +0530

    for gwt application to have common base url

---
+++ ixtlan-core/lib/ixtlan/child_path.rb
@@ -0,0 +1,18 @@
+module Ixtlan
+  class ChildPath
+    def initialize(app, rootpath)
+      @app = app
+      @rootpath = rootpath
+    end
+    
+    def call(env)
+      ['REQUEST_PATH','PATH_INFO','REQUEST_URI','SCRIPT_NAME'].each do |key|
+        if(env[key] =~ /[.]xml$/)
+          env[key].gsub!(/^\/#{@rootpath}/, "")
+        end
+      end
+      @app.call(env)
+    end
+  
+  end
+end
