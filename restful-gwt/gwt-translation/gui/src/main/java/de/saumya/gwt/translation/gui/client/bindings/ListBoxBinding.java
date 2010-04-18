03d24c697cefc559dda0a3aa06bef8bfd6c1b3a4
mkristian <m.kristian@web.de>
2010-04-18 09:08:22 +0530

    added multi select to listbox

--- restful-gwt/gwt-translation/gui/src/main/java/de/saumya/gwt/translation/gui/client/bindings/ListBoxBinding.java
+++ restful-gwt/gwt-translation/gui/src/main/java/de/saumya/gwt/translation/gui/client/bindings/ListBoxBinding.java
@@ -10,6 +10,7 @@ import com.google.gwt.user.client.ui.ListBox;
 
 import de.saumya.gwt.persistence.client.Resource;
 import de.saumya.gwt.persistence.client.ResourceCollection;
+import de.saumya.gwt.persistence.client.ResourceFactory;
 import de.saumya.gwt.translation.common.client.widget.ResourceCollectionResetable;
 import de.saumya.gwt.translation.common.client.widget.ResourceBindings.Binding;
 
@@ -17,6 +18,14 @@ public abstract class ListBoxBinding<T extends Resource<T>, S extends Resource<S
         extends ListBox implements Binding<T>, ResourceCollectionResetable<S> {
 
     private final Map<String, S> map = new HashMap<String, S>();
+
+    protected ListBoxBinding(final boolean multiselect) {
+        super(multiselect);
+    }
+
+    protected ListBoxBinding() {
+        super();
+    }
 
     public void reset(final ResourceCollection<S> resources) {
         clear();
@@ -26,10 +35,31 @@ public abstract class ListBoxBinding<T extends Resource<T>, S extends Resource<S
         }
     }
 
+    protected void selectAll(final ResourceCollection<S> resources) {
+        if (resources != null) {
+            for (int i = 0; i < getItemCount(); i++) {
+                setItemSelected(i,
+                                resources.contains(this.map.get(getValue(i))));
+            }
+        }
+    }
+
     protected S getResource() {
         final String key = getValue(getSelectedIndex());
         return this.map.get(key);
     }
+
+    protected ResourceCollection<S> getResources(
+            final ResourceFactory<S> factory) {
+        final ResourceCollection<S> result = new ResourceCollection<S>(factory);
+        for (int i = 0; i < getItemCount(); i++) {
+            if (isItemSelected(i)) {
+                result.add(this.map.get(getValue(i)));
+            }
+        }
+        return result;
+    }
+
     // protected void setResource(final T value) {
     // setItemSelected(index, selected)
     // final String val = value == null ? null : value.toString();
