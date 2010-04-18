f3831047bb110375cc19f8e5e64c7c275d99d0ac
mkristian <m.kristian@web.de>
2010-04-18 16:40:24 +0530

    added locales in configuration

---
+++ ixtlan-core/lib/ixtlan/controllers/child_references.rb
@@ -0,0 +1,21 @@
+module Ixtlan
+  module Controllers
+    module ChildReferences
+
+      def setup_children(new_children, old_children, model)
+        new_children ||= []
+        new_children = [new_children] unless new_children.is_a? Array
+        new_ids = new_children.collect { |v| v[:id].to_i }
+        old_children.select do |g|
+          !(new_ids.member? g.id)
+        end.each do |g|
+          old_children.delete(g)
+        end
+        old_ids = old_children.collect { |g| g.id }
+        new_ids.each do |gid|
+          old_children << model.get!(gid)  unless old_ids.member? gid
+        end
+      end
+    end
+  end
+end
--- ixtlan-core/lib/ixtlan/controllers/configurations_controller.rb
+++ ixtlan-core/lib/ixtlan/controllers/configurations_controller.rb
@@ -2,7 +2,14 @@ module Ixtlan
   module Controllers
     module ConfigurationsController
 
+      private
+
       CONFIGURATION = Object.full_const_get(::Ixtlan::Models::CONFIGURATION)
+      LOCALE = Object.full_const_get(::Ixtlan::Models::LOCALE)
+
+      include ChildReferences
+
+      public
 
       # GET /configuration
       # GET /configuration.xml
@@ -26,7 +33,9 @@ module Ixtlan
         @configuration = CONFIGURATION.instance
         @configuration.current_user = current_user
 
-        locales =  params[:configuration].delete(:locales)
+        locales =  params[:configuration].delete(:locales)[:locale]
+        setup_children(locales, @configuration.locales, LOCALE)
+
         respond_to do |format|
           if @configuration.update(params[:configuration]) or not @configuration.dirty?
             flash[:notice] = 'Configuration was successfully updated.'
