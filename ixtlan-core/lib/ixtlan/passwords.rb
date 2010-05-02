9ac4117bfcdd50c4fed445aefb1e26e333eab837
mkristian <m.kristian@web.de>
2010-05-02 16:47:26 +0530

    return nil it length is smaller than 1

--- ixtlan-core/lib/ixtlan/passwords.rb
+++ ixtlan-core/lib/ixtlan/passwords.rb
@@ -1,19 +1,21 @@
 module Ixtlan
   class Passwords
     def self.generate(length=64)
-      # A-Z starting with 97 and having 26 characters
-      # a-z starting with 65 and having 26 characters
-      # 0-9 starting with 48 and lies inside range starting with 33 and having 26 characters
-      offset=[97, 65, 33]
+      if length > 0
+        # A-Z starting with 97 and having 26 characters
+        # a-z starting with 65 and having 26 characters
+        # 0-9 starting with 48 and lies inside range starting with 33 and having 26 characters
+        offset=[97, 65, 33]
 
-      # collect random characters from the either of the above ranges
-      begin
-        pwd = (0..(length - 1)).collect do
-          j = ActiveSupport::SecureRandom.random_number(78)
-          (offset[j / 26] + (j % 26)).chr
-        end.join
-      end while !((pwd =~ /[a-z]/) && (pwd =~ /[A-Z]/) && (pwd =~ /[!-;]/))
-      pwd
+        # collect random characters from the either of the above ranges
+        begin
+          pwd = (0..(length - 1)).collect do
+            j = ActiveSupport::SecureRandom.random_number(78)
+            (offset[j / 26] + (j % 26)).chr
+          end.join
+        end while !((pwd =~ /[a-z]/) && (pwd =~ /[A-Z]/) && (pwd =~ /[!-;]/))
+        pwd
+      end
     end
   end
 end
