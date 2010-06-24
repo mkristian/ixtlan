64d361a06df83d0aaacb47e4f6533f026e0c9f17
mkristian <m.kristian@web.de>
2010-06-24 18:48:59 +0530

    more inline docu

--- ixtlan-core/lib/ixtlan/simple_client.rb
+++ ixtlan-core/lib/ixtlan/simple_client.rb
@@ -2,33 +2,43 @@ require 'net/http'
 module Ixtlan
   class NoAuthenticationError < StandardError; end
 
+  # restful XML client using HTTP (no HTTPS support !!)
   class SimpleClient
     
     def initialize(options)
       @host = options[:host]
-      @port = (options[:port] || 80).to_i
+      @port = (options[:port] || 80).to_i # defaults to 80
       @username = options[:username]
       @password = options[:password]
     end
 
-    # load resources has not payload 
-    # maybe a query which depends on the service
+    # restful get for the given path (like '/users.xml' or '/users/1.xml').
+    # which retrieves the data
+    # usually there is no payload - maybe a query if the service accepts it
     def get(path, payload = nil)
       req = Net::HTTP::Get.new(path)
       action(req, payload)
     end
 
+    # restful post for the given path (like '/users.xml').
+    # which creates a new resource or new resources (if the underlying 
+    # service provides bulk creation)
     def post(path, payload)
       req = Net::HTTP::Post.new(path)
       action(req, payload)
     end
 
+    # restful put for the given path (like '/users/1.xml').
+    # which updates a given resource or many resources (if the underlying 
+    # service provides bulk updates - path like '/users.xml')
     def put(path, payload)
       req = Net::HTTP::Put.new(path)
       action(req, payload)
     end
 
-    # delete a single resource has no payload
+    # restful delete for the given path (like '/users/1.xml').
+    # delete a single resource (with no payload) or many resources 
+    # (if the underlying service provides bulk updates - path like '/users.xml')
     def delete(path, payload = nil)
       req = Net::HTTP::Delete.new(path)
       action(req, payload)
@@ -57,20 +67,23 @@ module Ixtlan
       req['Authentication-Token'] = @token
       req['Cookie'] = @cookie
 
-      result = nil
       http = Net::HTTP.new(@host, @port)
       http.read_timeout = 999
       
+      result = nil
       response = http.start do |h|
         h.request(req) do |response|
           if response.kind_of?(Net::HTTPSuccess)
-            # in case there is a new token
+            # in case there is a new token keep it for the next request
             @token = response['Authentication-Token'] || @token
             # read result
             result = response.read_body
           elsif response.kind_of?(Net::HTTPUnauthorized)
+            # clear session tracking data
             @token = nil
             @cookie = nil
+            # this exception indicates authentication problem and
+            # can trigger retry
             raise NoAuthenticationError.new
           else
             # TODO maybe better error handling
@@ -85,6 +98,8 @@ module Ixtlan
       # setup request with authencitation.xml
       req = Net::HTTP::Post.new("/authentication.xml")
       req['Content-Type'] = 'application/xml'
+
+      # the login payload
       req.body = "<authentication><login>#{@username}</login><password>#{@password}</password></authentication>"
       req.content_length = req.body.size 
 
@@ -94,11 +109,11 @@ module Ixtlan
       response = http.start do |h|
         h.request(req) do |response|
           if response.kind_of?(Net::HTTPSuccess)
-            # read body to close connection
+            # read body to close connection - be nice client
             response.read_body
             # keep session tracking info for further request
             @token = response['Authentication-Token']
-            # take on the session_id part of the cookie
+            # take only the session_id part of the cookie
             @cookie = response['Set-Cookie'].sub(/;.*/, '')
           else
             # TODO maybe better error handling
